0d584ab6c103c46e16b93484609400e11dfbc0f1
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-03-21 18:26:39 -0600

    dummies categoricals

--- Task1_1.py
+++ Task1_1.py
@@ -6,7 +6,7 @@ from sklearn.decomposition import PCA
 
 # Carga del dataset
 data = pd.read_csv('bank_transactions.csv')
-print(data.head())
+#print(data.head()) 
 
 
 class KMeans:
@@ -53,34 +53,33 @@ class KMeans:
 # Eliminación de la columna de categoría, porque la columna TransactionID y CustumerID no tienen datos relevantes al analisis de datos.
 data = data.drop('TransactionID', axis=1)
 data = data.drop('CustomerID', axis=1)
+data = data.drop('CustomerDOB', axis =  1)
 
 
-# Convertir las columnas 'CustomerDOB' y 'TransactionDate' a valores numéricos
+""" # Convertir las columnas 'CustomerDOB' y 'TransactionDate' a valores numéricos
 data['CustomerDOB'] = pd.to_datetime(data['CustomerDOB'], format='%d/%m/%Y', errors='coerce')
-data['CustomerDOB'] = data['CustomerDOB'].fillna(0).apply(lambda x: x.toordinal())
+data['CustomerDOB'] = data['CustomerDOB'].fillna(0).apply(lambda x: x.toordinal()) """
 
 data['TransactionDate'] = pd.to_datetime(data['TransactionDate']).apply(lambda x: x.toordinal())
 
 pca = PCA(n_components=2)
 
 # Convertir las columnas restantes en variables binarias
-data_transformed = pd.get_dummies(data, columns=['CustGender', 'CustLocation'])
-data_transformed = pca.fit_transform(data_transformed)
-
-
-# Transformación PCA
-data_transformed = pca.fit_transform(data)
-
+categorical = ['CustGender', 'CustLocation']
+new_cols = pd.get_dummies(data[categorical])
+data = pd.concat([data.drop(categorical, axis=1), new_cols], axis = 1)
+print(data)
+data = pca.fit_transform(data)
 
 # Ajuste del modelo K-Means
 kmeans = KMeans(n_clusters=3)
-kmeans.fit(data_transformed)
-labels = kmeans.predict(data_transformed)
+kmeans.fit(data)
+labels = kmeans.predict(data)
 
 # Visualización de los clusters
 colors = ['r', 'g', 'b']
-for i in range(len(data_transformed)):
-    plt.scatter(data_transformed[i, 0], data_transformed[i, 1], color=colors[labels[i]])
+for i in range(len(data)):
+    plt.scatter(data[i, 0], data[i, 1], color=colors[labels[i]])
 plt.scatter(kmeans.centroids[0][0], kmeans.centroids[0][1], marker='*', s=200, color='black')
 plt.scatter(kmeans.centroids[1][0], kmeans.centroids[1][1], marker='*', s=200, color='black')
 plt.scatter(kmeans.centroids[2][0], kmeans.centroids[2][1], marker='*', s=200, color='black')
